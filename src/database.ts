db84f6c341c1902a870dacd1247e887daddebb48
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-09 21:46:59 -0300

    creating first migration

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,12 @@ import { Knex, knex as setupKnex } from 'knex';
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
   },
 };
 
--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server Running');
+    console.log('HTTP server Running a');
   });
