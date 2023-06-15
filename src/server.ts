85b982a981e30f4fd44f26a06e16332c20d98e7d
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-15 13:12:04 -0300

    handling env with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
@@ -10,7 +11,7 @@ app.get('/hello', async () => {
 });
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server Running');
