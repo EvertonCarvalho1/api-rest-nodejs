93474eb3cf6e5fd23906cf5caacd16e253ad8cbb
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-19 22:18:57 -0300

    creation of transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import crypto from 'node:crypto';
 import { knex } from '../database';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions').select('*');
-
-    return transactions;
+  app.post('/transactions', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    );
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+    return reply.status(201).send();
   });
 }
