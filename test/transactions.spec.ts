a8471a80796769b0cd565c1ca0f5e9d5f99bd572
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-23 20:04:46 -0300

    testing transaction listing

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,8 +1,9 @@
 import {
-  test,
+  it,
   beforeAll,
   afterAll,
   describe,
+  expect,
 } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
@@ -16,7 +17,7 @@ describe('Transaction routes', () => {
     await app.close();
   });
 
-  test('User can create a new transaction', async () => {
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -25,5 +26,38 @@ describe('Transaction routes', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(
+      app.server,
+    )
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      });
+
+    const cookies =
+      createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(
+      app.server,
+    )
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(
+      listTransactionsResponse.body.transactions,
+    ).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ]);
+
+    // console.log(listTransactionsResponse.body);
   });
 });
