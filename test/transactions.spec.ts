f135333132749ce8a3a002324d1de12766dfaa6d
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-23 19:43:56 -0300

    categorizing the tests

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,22 +1,29 @@
-import { test, beforeAll, afterAll } from 'vitest';
+import {
+  test,
+  beforeAll,
+  afterAll,
+  describe,
+} from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
 
-beforeAll(async () => {
-  await app.ready();
-});
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
 
-afterAll(async () => {
-  await app.close();
-});
+  afterAll(async () => {
+    await app.close();
+  });
 
-test('O usuário consegue criar uma nova transação', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201);
+  test('User can create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201);
+  });
 });
