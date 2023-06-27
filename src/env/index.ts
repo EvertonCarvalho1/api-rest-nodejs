f4085e87303795bc6d3a45512a7ee679c90aa761
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2023-06-27 09:26:28 -0300

    deploy the app on render

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,8 +11,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 const _env = envSchema.safeParse(process.env);
